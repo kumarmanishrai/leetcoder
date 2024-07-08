d3ca4cd1d37cc1fb97c75d0a1d472ff976775ec8
Manish Kumar Rai <98271764+kumarmanishrai@users.noreply.github.com>
2024-07-09 02:06:10 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(express.json())
 app.use(cors({
-    origin: ["https://leetcoder-2c2q.vercel.app"],
+    origin: ["https://leetcoder-umber.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization'],
